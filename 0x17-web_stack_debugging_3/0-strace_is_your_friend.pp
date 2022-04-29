becff089191b3ce7ab18084549c71857d8d11fe6
Waython Yesse <waythonny@yahoo.com>
2022-04-29 09:07:12 +0300

    Hello darkness my old friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fixes the 500 error.
+# Fixes the 500 error at wordpress site.
 exec { '/bin/bash':
   command => 'sudo mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
   creates => '/var/www/html/wp-includes/class-wp-locale.phpp',
