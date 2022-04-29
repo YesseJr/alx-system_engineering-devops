ca565781c1d0a8e3e5967d1ad4a99ad2907fea7a
Waython Yesse <waythonny@yahoo.com>
2022-04-29 09:05:36 +0300

    Hello darkness my old friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,11 @@
 # Fixes the 500 error.
-exec { 'fix-wordpress':
-  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
-/var/www/html/wp-settings.php; service apache2 restart"',
-  path    => '/usr/bin:/usr/sbin:/bin'
+exec { '/bin/bash':
+  command => 'sudo mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
+  creates => '/var/www/html/wp-includes/class-wp-locale.phpp',
+  path    => ['/usr/bin', '/usr/sbin'],
+}
+
+service { 'apache2':
+  ensure  => running,
+  restart => 'sudo service apache2 restart',
 }
