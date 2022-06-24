23228a63ffc5cad09bd98b05fa6f2e19c991e878
YesseJr <waythonny@yahoo.com>
2022-06-24 07:28:07 +0300

    Too late

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,39 +1,14 @@
 #!/usr/bin/python3
-"""
-    Takes an argument passed to the script to be used in an API query.
-    Retrieved data is then printed in a formatted way.
-"""
-import json
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        url = 'https://jsonplaceholder.typicode.com/'
-        user_resp = requests.get(url + 'users/{}'.format(sys.argv[1]))
-        todo_resp = requests.get(url + 'todos?userId={}'.format(sys.argv[1]))
-        if all([user_resp, todo_resp]) is not None:
-            try:
-                user_d = json.loads(user_resp.text)
-            except JSONDecodeError:
-                user_d = {}
-            try:
-                todo_l = json.loads(todo_resp.text)
-            except JSONDecodeError:
-                todo_l = []
-            tasks = ''
-            completed = 0
-            total = 0
-            if type(todo_l) is list:
-                for todo in todo_l:
-                    if type(todo) is not dict:
-                        continue
-                    if todo.get('completed') is True:
-                        tasks += '\t {}\n'.format(todo.get('title'))
-                        completed += 1
-                total = len(todo_l)
-            if type(user_d) is dict and user_d != {}:
-                employee = 'Employee {} is done with tasks({}/{}):\n'\
-                    .format(user_d.get('name'), completed, total)
-                print(employee + tasks, end='')
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
