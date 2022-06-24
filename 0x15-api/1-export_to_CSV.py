e2933ca8a2c64aea0b2e35cc176ed76cbd4742b2
YesseJr <waythonny@yahoo.com>
2022-06-24 08:32:31 +0300

    Make it executable

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,39 +1,27 @@
 #!/usr/bin/python3
-"""
-Request from API; Return TODO list progress given employee ID
-Export this data to CSV
-"""
-from sys import argv
-import csv
+"""gets todo info and export it to a csv file"""
 import requests
+import sys
 
 
-
-def to_csv():
-    """return API data"""
-    users = requests.get("http://jsonplaceholder.typicode.com/users")
-    for u in users.json():
-        if u.get('id') == int(argv[1]):
-            USERNAME = (u.get('username'))
-            break
-    TASK_STATUS_TITLE = []
-    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
-    for t in todos.json():
-        if t.get('userId') == int(argv[1]):
-            TASK_STATUS_TITLE.append((t.get('completed'), t.get('title')))
-
-    """export to csv"""
-    filename = "{}.csv".format(argv[1])
-    with open(filename, "w") as csvfile:
-        fieldnames = ["USER_ID", "USERNAME",
-                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                                quoting=csv.QUOTE_ALL)
-        for task in TASK_STATUS_TITLE:
-            writer.writerow({"USER_ID": argv[1], "USERNAME": USERNAME,
-                             "TASK_COMPLETED_STATUS": task[0],
-                             "TASK_TITLE": task[1]})
+def export_todo(id):
+    """exports todo to a csv"""
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    user = requests.get(user_url).json()
+    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+    todos = requests.get(todo_url).json()
+    user_name = user["username"]
+    file_name = "{}.csv".format(id)
+    with open(file_name, "w") as f:
+        for todo in todos:
+            row = '"{}","{}","{}","{}"\n'.format(
+                id,
+                user_name,
+                str(todo["completed"]),
+                todo["title"]
+            )
+            f.write(row)
 
 
 if __name__ == "__main__":
-    to_csv()
+    export_todo(sys.argv[1])
